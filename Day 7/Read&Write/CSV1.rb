231fe7a5ea2611df3c1b03d6939c6b84073be641
Santu <santu@corephp.com>
2022-09-20 18:51:01 +0530

    commited

--- Day 7/Read&Write/CSV1.rb
+++ Day 7/Read&Write/CSV1.rb
@@ -2,7 +2,7 @@
 
 require 'csv'
 
-file = CSV.parse(File.read("/home/ubuntu/Documents/Ruby Ravindra/Day 7/Name.csv"))
+file = CSV.parse(File.read("/home/ubuntu/Documents/Ruby Ravindra/Day 7/Read&Write/Name.csv"))
 
 file.each do |row|
   print row
@@ -10,6 +10,10 @@ file.each do |row|
 end
 
 # puts file.to_h
+file1 = file.to_h
+puts "#{file1}"
 
 # Accessing the second row
 print "The First person Details are : #{file[1]}"
+
+
