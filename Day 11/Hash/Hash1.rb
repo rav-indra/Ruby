76cbc9a8914a70434ecdb807142c92da7ba498e0
Santu <santu@corephp.com>
2022-09-26 10:27:30 +0530

    Commited

---
+++ Day 11/Hash/Hash1.rb
@@ -0,0 +1,21 @@
+# array and hash
+
+food = ["Orange", "Bacon", "Apple"]
+
+puts food.first
+puts food.last
+
+food << "Yogurt"
+
+puts "#{food}"
+
+
+# country name hash with country code
+
+hs = {}
+hs["India"] = "IN"
+puts hs
+hs["United State of America"] = "US"
+puts hs
+hs["Russian Federation"] = "RU"
+puts hs
