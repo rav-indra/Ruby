45c6e905318d3b63e0522c7aad914ac4fae25264
Santu <santu@corephp.com>
2022-11-08 18:42:59 +0530

    Commited

---
+++ Day 33/Starts/demo.js
@@ -0,0 +1,17 @@
+// it will change paragraph and pop alert box
+function change() {
+  document.getElementById("inner").innerHTML = "Paragraph Changed"
+  window.alert("This is windows alert")
+}
+// it will pop alert box
+function alertt() {
+  window.alert("This is windows alert")
+}
+// it will write '1' each time when mouse arrow moves on button
+function consl() {
+  console.log(1)
+}
+// it will write "Hellooooo" on page , deleting all other html
+function writee() {
+  document.write("Hellooooo")
+}
