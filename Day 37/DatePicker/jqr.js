b92f3e07ff3b44cbbfda7285e7a48a95bd357f07
Santu <santu@corephp.com>
2022-11-14 18:59:56 +0530

    Commited

---
+++ Day 37/DatePicker/jqr.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#date").datepicker({
+    dateFormat: 'dd-mm-yy',
+    showOn: 'button',
+    showWeek: true,
+    buttonText: 'Calender',
+    // numberOfMonths: 3,
+    changeMonth: true,
+    changeYear: true,
+    // minDate: new Date(2020, 3, 20),
+    // maxDate: new Date(2022, 10, 1)
+    // showAnim: 'blind',
+    // duration: 1000,
+    // defaultDate: new Date(2020, 0, 1),
+    nextText: 'forward',
+    prevText: 'backward',
+    onSelect: function(dateText) {
+      alert(dateText)
+    },
+    buttonImage: './icon.png',
+    buttonImageOnly: true,
+    firstDay: 0,
+    // showMonthAfterYear: true,
+    weekHeader: "Week",
+    // stepMonths: 3
+  });
+});
